7fd1f301b4d6f3786537dde7e8b7eab2a8cb0006
tamir <tamiroffen@gmail.com>
2024-11-24 16:46:06 +0200

    run_multi_channel.bat HTM_Results files, so no need to run the swat_htm.py parts again

--- run_multi_channel.bat
+++ run_multi_channel.bat
@@ -1,5 +1,6 @@
 :: encoding_type = raw, encoding_duration_enabled = False, 
 :: sbp = False, hierarchy_lvl = number of channels
+:: order of channels matters!!!
 
 @REM python swat_htm.py --stages_channels ^
 @REM     P1:LIT101:window=5,sdr_size=1024 ^
@@ -9,6 +10,7 @@ python calc_anomaly_stats.py -sn MC -esn MC -ofa _multi_channel --stages_channel
     P1:LIT101 ^
     P2:AIT202
     
+
 @REM python swat_htm.py --stages_channels ^
 @REM     P5:FIT503:window=21,sdr_size=2048 ^
 @REM     P5:PIT501:window=5,sdr_size=1024 ^
@@ -18,5 +20,27 @@ python calc_anomaly_stats.py -sn MC -esn MC -ofa _multi_channel --stages_channel
     P5:FIT503 ^
     P5:PIT501 ^
     P5:PIT503
+
+
+@REM python swat_htm.py --stages_channels ^
+@REM     P5:FIT501:window=21,sdr_size=1024 ^
+@REM     P5:PIT501:window=5,sdr_size=1024 ^
+@REM     P5:AIT503:window=34,sdr_size=512
+
+python calc_anomaly_stats.py -sn MC -esn MC -ofa _multi_channel --stages_channels ^
+    P5:FIT501 ^
+    P5:PIT501 ^
+    P5:AIT503
+
+
+@REM python swat_htm.py --stages_channels ^
+@REM     P3:DPIT301:window=21,sdr_size=512 ^
+@REM     P3:FIT301:window=34,sdr_size=512 ^
+@REM     P3:LIT301:window=1,sdr_size=512
+
+python calc_anomaly_stats.py -sn MC -esn MC -ofa _multi_channel --stages_channels ^
+    P3:DPIT301 ^
+    P3:FIT301 ^
+    P3:LIT301   
 
 python calc_anomaly_stats.py -ofa _multi_channel --final_stage 
